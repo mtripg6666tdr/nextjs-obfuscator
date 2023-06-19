e72ed21a8ce5b4d14336328106fe6b5eb55f2b5c
うさみょん(myon2019)/mtripg6666tdr <56076195+mtripg6666tdr@users.noreply.github.com>
2023-06-19 16:04:30 +0900

    Fix(typing): fix wrong typing

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,48 +1,52 @@
 declare module "nextjs-obfuscator" {
   import type { ObfuscatorOptions } from "javascript-obfuscator";
-  type customHandler = (path:string)=>boolean;
+
+  type customHandler = (path: string) => boolean;
+
   type pluginOptions = {
     /**
      * This handler determines which chunks will be obfuscated or not. We do not recommend to use this arg.  
      * @deprecated this option is for compatiblity witch v1.0.0
      */
-    customHandler:customHandler,
+    customHandler: customHandler,
     /**
      * custom regular expression to be used to obfuscate custom files
      */
-    customMatch:RegExp,
+    customMatch: RegExp,
     /**
      * determines which files to be obfuscated
      */
-    obfuscateFiles:{
+    obfuscateFiles: Partial<{
       main: boolean,
       app: boolean,
       error: boolean,
-      pages: boolean|string[],
+      pages: boolean | string[],
       webpack: boolean,
       framework: boolean,
       buildManifest: boolean,
-    },
+    }>,
     /**
      * indicates whether the plugin logs to console
      */
-    log:boolean,
+    log: boolean,
   };
+
   class NextJSBundleObfuscatorPlugin {
     /**
      * initialize plugin
      * @param options options that will be passed to javascript-obfuscator
      * @param pluginOptions Plugin options that determines, which files to be obfuscated, and so on
      */
-    constructor(options:ObfuscatorOptions, pluginOptions?:pluginOptions);
+    constructor(options: ObfuscatorOptions, pluginOptions?: Partial<pluginOptions>);
     /**
      * @deprecated
      */
-    constructor(options:ObfuscatorOptions, customHandler?:customHandler);
+    constructor(options: ObfuscatorOptions, customHandler?: customHandler);
     /**
      * @internal
      */
-    apply(compiler:any):void;
+    apply(compiler: any): void;
   }
+
   export = NextJSBundleObfuscatorPlugin;
 }
